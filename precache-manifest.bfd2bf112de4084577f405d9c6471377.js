d74b7f4ea6569cd1fa676024434133091a3bb78d
Daria Yeromina <daria.yeromina@wiosna.org.pl>
2020-06-07 23:40:44 +0200

    Updates

---
+++ precache-manifest.bfd2bf112de4084577f405d9c6471377.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4970788b60589986f7deaf1a75153b62",
+    "url": "/Palindrom/index.html"
+  },
+  {
+    "revision": "d8813d200b4bc969df6b",
+    "url": "/Palindrom/static/js/2.5f87ba2a.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/Palindrom/static/js/2.5f87ba2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6226c33aaa63c65249f",
+    "url": "/Palindrom/static/js/main.520eb9d4.chunk.js"
+  },
+  {
+    "revision": "46e0626a6614a7bb4c40",
+    "url": "/Palindrom/static/js/runtime-main.36ef94ea.js"
+  },
+  {
+    "revision": "39f7a838e5d7db7a598f6b857baae852",
+    "url": "/Palindrom/static/media/check.39f7a838.svg"
+  },
+  {
+    "revision": "e86bbe85243af092a9736cd48e183870",
+    "url": "/Palindrom/static/media/reject.e86bbe85.svg"
+  }
+]);
