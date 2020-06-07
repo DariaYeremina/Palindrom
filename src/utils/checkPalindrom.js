16b960720cb3cb55fda36408cf90d67f3ef5e39f
DariaYeremina <dariaeremina12@gmail.com>
2020-06-07 22:11:04 +0200

    dd tests for checkPalindrom function

--- src/utils/checkPalindrom.js
+++ src/utils/checkPalindrom.js
@@ -2,16 +2,23 @@ const clean = (phrase) => phrase.toLowerCase().replace(/[^a-z0-9ąęóżźńł]+
 
 export const checkPalindrom = (phrase) => {
   if (typeof (phrase) === 'string') {
-    const cleanPhrase = clean(phrase);
-    for (let i = 0; i < cleanPhrase.length; i++) {
-      if (cleanPhrase[i] !== cleanPhrase[cleanPhrase.length - 1 - i]) {
+    if (phrase.length === 0) {
+      throw new Error("It's an empty string!");
+    } else {
+      const cleanPhrase = clean(phrase);
+      if (cleanPhrase.length === 0) {
         return false;
       }
-      if (i >= cleanPhrase.length - 1 - i) {
-        return true;
+      for (let i = 0; i < cleanPhrase.length; i++) {
+        if (cleanPhrase[i] !== cleanPhrase[cleanPhrase.length - 1 - i]) {
+          return false;
+        }
+        if (i >= cleanPhrase.length - 1 - i) {
+          return true;
+        }
       }
+      return true;
     }
-    return true;
   }
   throw new Error("It isn't a string!");
 };
